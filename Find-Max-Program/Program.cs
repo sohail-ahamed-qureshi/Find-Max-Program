b47746102f3f49fe10e1ab11f7f4ebaef6173dc6
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-09 15:41:49 +0530

    [sohail] Add: Welcome message for Test cases of Find maximum numbers

---
+++ Find-Max-Program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Find_Max_Program
+{
+    class Program
+    {
+        /// <summary>
+        /// Find Maximum program with test cases.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum of Numbers program");
+        }
+    }
+}
